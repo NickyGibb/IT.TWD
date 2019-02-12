a212d459002e04c63383ed8250d5057445a21b52
nicholasmgibb@gmail.com <nicholasmgibb@gmail.com>
2019-02-12 17:22:24 +0000

    too many directories which caused a big rework of my files

--- rango/admin.py
+++ rango/admin.py
@@ -2,6 +2,20 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from rango.models import Category, Page
+from rango.models import UserProfile
+
+class CategoryAdmin(admin.ModelAdmin):
+	prepopulated_fields ={'slug':('name',)}
+
+class PageAdmin(admin.ModelAdmin):
+	list_display = ('title','category','url')
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
+
 
 
 # Register your models here.
--- rango/apps.py
+++ rango/apps.py
@@ -4,6 +4,11 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-
 class RangoConfig(AppConfig):
     name = 'rango'
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title','category','url')
+
+admin.site.register(Page,PageAdmin)
+    
