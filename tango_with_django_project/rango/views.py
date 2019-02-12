f258b0d44bdf53d6740c5cdc3f9d8d6c0b921a99
nicholasmgibb@gmail.com <nicholasmgibb@gmail.com>
2019-02-12 10:04:41 +0000

    chapter 9 end

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -9,7 +9,9 @@ from rango.forms import CategoryForm
 from rango.forms import PageForm
 from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import logout
+from django.contrib.auth import login, logout
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -96,12 +98,34 @@ def register(request):
 
     return render(request, 'rango/register.html',{ 'user_form':user_form,'profile_form':profile_form, 'registered':registered})
 
+def user_login(request):
+    context = RequestContext(request)
+
+
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/rango/')
+            else:
+                return HttpResponse("Your Rango account is disabled")
+
+        else:
+            print "Invalid login details: {0}, {1}".format(username,password)
+            context_dict = { 'bad details': "Invalid Login Details supplied"}
+            return render_to_response('rango/login.html', context_dict, context)
+    else:
+        return render_to_response('rango/login.html', {}, context)
 
 @login_required
 def restricted(request):
     return HttpResponse("Since you're logged in, you can see this text!")
 
-
+@login_required
 def user_logout(request):
     logout(request)
     return HttpResponseRedirect(reverse('index'))
